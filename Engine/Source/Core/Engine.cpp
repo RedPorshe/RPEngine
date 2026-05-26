95702b9d1b8235229617c949ca4b2f1771cbfa80
Константин Александрович Чудинов <guger58@GMAIL.com>
2026-05-27 04:30:09 +0600

    Added define ENGINE_NAME

--- Engine/Source/Core/Engine.cpp
+++ Engine/Source/Core/Engine.cpp
@@ -8,7 +8,7 @@ DEFINE_LOG_CATEGORY_STATIC(EngineLog);
 
 Engine::Engine()
 {
-    RP_LOG(EngineLog, Display, "Initializing Engine, version: {}", version());
+    RP_LOG(EngineLog, Display, "Initializing {}, version: {}", ENGINE_NAME, version());
     RP_LOG(EngineLog, Warning, "Adsada");
     RP_LOG(EngineLog, Log, "Adsada");
     RP_LOG_DEBUG(EngineLog, Warning, "Adsada");
