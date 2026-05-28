8365da9d3a228fc1e4af0f5f0028635738ad7855
Константин Александрович Чудинов <guger58@GMAIL.com>
2026-05-29 01:55:00 +0600

    Update Event.h

--- Engine/Source/Event/Event.h
+++ Engine/Source/Event/Event.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include "Log/Log.h"
+#include "InputEvent.h"
 
 DEFINE_LOG_CATEGORY_STATIC(LogEvent);
 
@@ -11,6 +12,23 @@ template <typename... Args>
 class Event
 {
 public:
-    void invoke(Args... args) { RP_LOG(LogEvent, Display, "Dispatch event"); }
+    void invoke(Args... args)
+    {
+        auto printEvent = [](const InputEvent& event)
+        {
+            std::string eventStr{};
+            switch (event.type)
+            {
+                case EventType::WindowClose: eventStr = "Window Close"; break;
+                case EventType::WindowResize: eventStr = "Window Resize"; break;
+                case EventType::MouseMove: eventStr = "Mouse Move"; break;
+                case EventType::MouseButton: eventStr = "Mouse Button"; break;
+                case EventType::MouseScroll: eventStr = "Mouse Scroll"; break;
+                case EventType::KeyPress: eventStr = "Key Press"; break;
+            }
+            RP_LOG(LogEvent, Display, "Dispatch event: {}", eventStr);
+        };
+        (printEvent(args),...);
+    }
 };
 }  // namespace RPE
